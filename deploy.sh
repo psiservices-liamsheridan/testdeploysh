fb177c9bbe158db3ddc3fff1052d6ecece0e5e40
psiservices-liamsheridan <49398159+psiservices-liamsheridan@users.noreply.github.com>
2020-02-03 10:41:42 +0000

    Update deploy.sh

--- deploy.sh
+++ deploy.sh
@@ -1,9 +1,8 @@
-"#!/bin/bash -ex\n",
-"mkdir -p /home/ubuntu/.aws\n",
-"mkdir -p /home/ubuntu/deployment/configs\n",
-"mkdir -p /home/ubuntu/deployment/scripts\n",
-"wget -q https://packages.microsoft.com/config/ubuntu/18.04/packages-microsoft-prod.deb\n",
-"dpkg -i packages-microsoft-prod.deb\n",
-"apt-get update\n",
-"DEBIAN_FRONTEND=noninteractive apt-get -y install python-setuptools apt-transport-https dotnet-sdk-3.0 nginx unzip rename\n"
+#!/bin/bash -ex\n,
+mkdir -p /home/ubuntu/deployment/configs\n,
+"mkdir -p /home/ubuntu/deployment/scripts\n,
+wget -q https://packages.microsoft.com/config/ubuntu/18.04/packages-microsoft-prod.deb\n,
+dpkg -i packages-microsoft-prod.deb\n,
+apt-get update\n,
+DEBIAN_FRONTEND=noninteractive apt-get -y install python-setuptools apt-transport-https dotnet-sdk-3.0 nginx unzip rename
 
