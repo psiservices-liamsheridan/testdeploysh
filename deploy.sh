6a212b68d36103cf577bd3508d42210be983b7aa
psiservices-liamsheridan <49398159+psiservices-liamsheridan@users.noreply.github.com>
2020-02-03 10:35:10 +0000

    Add files via upload

---
+++ deploy.sh
@@ -0,0 +1,9 @@
+"#!/bin/bash -ex\n",
+"mkdir -p /home/ubuntu/.aws\n",
+"mkdir -p /home/ubuntu/deployment/configs\n",
+"mkdir -p /home/ubuntu/deployment/scripts\n",
+"wget -q https://packages.microsoft.com/config/ubuntu/18.04/packages-microsoft-prod.deb\n",
+"dpkg -i packages-microsoft-prod.deb\n",
+"apt-get update\n",
+"DEBIAN_FRONTEND=noninteractive apt-get -y install python-setuptools apt-transport-https dotnet-sdk-3.0 nginx unzip rename\n"
+
